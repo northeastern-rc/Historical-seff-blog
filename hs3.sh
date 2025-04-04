0b3fab3a60f98bd160ee0e58b47eeabc23217eb7
Tara Ketchem <101415920+tketchem14@users.noreply.github.com>
2025-04-04 16:30:27 -0400

    Create hs3.sh

---
+++ hs3.sh
@@ -0,0 +1,11 @@
+[user@login-00 ~] seff 38391902
+Job ID: 38391902
+Cluster: discovery
+User/Group: user/users
+State: COMPLETED (exit code 0)
+Cores: 1
+CPU Utilized: 00:00:00
+CPU Efficiency: 0.00% of 00:03:25 core-walltime
+Job Wall-clock time: 00:03:25
+Memory Utilized: 652.00 KB
+Memory Efficiency: 0.03% of 1.95 GB
